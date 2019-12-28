2382a9a38b6968942bfecef5fe552b97082fc391
Olivia <olivia_github@outlook.com>
2019-12-28 18:48:20 +0000

    Adding intro

---
+++ code/rust-sokoban-01/src/main.rs
@@ -0,0 +1,35 @@
+use ggez;
+use ggez::{conf, event, Context, GameResult};
+
+// This struct will hold all our game state
+// For now there is nothing to be held, but we'll add
+// things shortly.
+struct Game {}
+
+// This is the main event loop. ggez tells us to implement
+// two things:
+// - updating
+// - rendering
+impl event::EventHandler for Game {
+    fn update(&mut self, _context: &mut Context) -> GameResult {
+        // TODO: update game logic here
+        Ok(())
+    }
+
+    fn draw(&mut self, _context: &mut Context) -> GameResult {
+        // TODO: update draw here
+        Ok(())
+    }
+}
+
+pub fn main() -> GameResult {
+    // Create a game context and event loop
+    let context_builder = ggez::ContextBuilder::new("rust_sokoban", "sokoban")
+        .window_setup(conf::WindowSetup::default().title("Rust Sokoban!"))
+        .window_mode(conf::WindowMode::default().dimensions(800.0, 600.0));
+    let (context, event_loop) = &mut context_builder.build()?;
+    // Create the game state
+    let game = &mut Game {};
+    // Run the main event loop
+    event::run(context, event_loop, game)
+}
