883156aac8fec8cfe60d0c6a886c02fb95074c65
Olivia <olivia_github@outlook.com>
2020-05-30 21:22:20 +0100

    Add delta time and animations to rendering system

--- code/rust-sokoban-c03-02/src/systems/rendering_system.rs
+++ code/rust-sokoban-c03-02/src/systems/rendering_system.rs
@@ -8,6 +8,7 @@ use ggez::graphics::Image;
 use ggez::nalgebra as na;
 use ggez::Context;
 use specs::{Join, Read, ReadStorage, System};
+use std::time::Duration;
 
 pub struct RenderingSystem<'a> {
     pub context: &'a mut Context,
@@ -29,6 +30,36 @@ impl RenderingSystem<'_> {
         )
         .expect("expected drawing queued text");
     }
+
+    pub fn get_image(&mut self, renderable: &Renderable, delta: Duration) -> Image {
+        println!("delta MS {}", delta.as_millis());
+        println!("renderable.paths.len() {}", renderable.paths.len());
+
+        let image_index = if renderable.paths.len() == 1 {
+            // we only have one image, so we just return that
+            0
+        } else {
+            // If we have multiple, we want to select the right one based on
+            // how much time has passed since last time we rendered (delta time).
+            // We wil split each second into 4 frames (a new frame every 250ms).
+            // So if we have 4 frames, it will be 1, 2, 3, 4 (in one second)
+            // So if we have 3 frames, it will be 1, 2, 3, 1 (in one second)
+            let frame_index = (delta.as_millis() % 1000) / 250;
+            println!("frame_index {}", frame_index);
+
+            // frame_index will be a number between 0 and 4, if we have less than 4
+            // frames we need to make sure that wraps around, so we mod by how many
+            // paths we actually have.
+            let path_index = (frame_index as usize) % renderable.paths.len();
+            println!("path_index {}", path_index);
+
+            path_index
+        };
+
+        let image_path = renderable.paths[image_index].clone();
+
+        Image::new(self.context, image_path).expect("expected image")
+    }
 }
 
 // System implementation
@@ -36,12 +67,13 @@ impl<'a> System<'a> for RenderingSystem<'a> {
     // Data
     type SystemData = (
         Read<'a, Gameplay>,
+        Read<'a, Time>,
         ReadStorage<'a, Position>,
         ReadStorage<'a, Renderable>,
     );
 
     fn run(&mut self, data: Self::SystemData) {
-        let (gameplay, positions, renderables) = data;
+        let (gameplay, time, positions, renderables) = data;
 
         // Clearing the screen (this gives us the backround colour)
         graphics::clear(self.context, graphics::Color::new(0.95, 0.95, 0.95, 1.0));
@@ -55,8 +87,7 @@ impl<'a> System<'a> for RenderingSystem<'a> {
         // and draw it at the specified position.
         for (position, renderable) in rendering_data.iter() {
             // Load the image
-            let image = Image::new(self.context, renderable.paths.first().unwrap().clone())
-                .expect("expected image");
+            let image = self.get_image(renderable, time.delta);
             let x = position.x as f32 * TILE_WIDTH;
             let y = position.y as f32 * TILE_WIDTH;
 
