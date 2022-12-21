432586887deff35e2a4272054aeec31265cb4568
Mateusz Kwiatkowski <kwiat1990@gmail.com>
2022-12-21 16:45:58 +0100

    update code and book for chapter 2.4

--- code/rust-sokoban-c02-04/src/systems/rendering_system.rs
+++ code/rust-sokoban-c02-04/src/systems/rendering_system.rs
@@ -1,11 +1,12 @@
-
-
 use crate::components::*;
 use crate::constants::TILE_WIDTH;
 
-use ggez::{Context, graphics::{self, DrawParam, Image}};
-use specs::{Join, ReadStorage, System};
+use ggez::{
+    graphics::{self, Canvas, DrawParam, Image},
+    Context,
+};
 use glam::Vec2;
+use specs::{Join, ReadStorage, System};
 
 pub struct RenderingSystem<'a> {
     pub context: &'a mut Context,
@@ -20,7 +21,8 @@ impl<'a> System<'a> for RenderingSystem<'a> {
         let (positions, renderables) = data;
 
         // Clearing the screen (this gives us the backround colour)
-        graphics::clear(self.context, graphics::Color::new(0.95, 0.95, 0.95, 1.0));
+        let mut canvas =
+            Canvas::from_frame(self.context, graphics::Color::new(0.95, 0.95, 0.95, 1.0));
 
         // Get all the renderables with their positions and sort by the position z
         // This will allow us to have entities layered visually.
@@ -31,17 +33,18 @@ impl<'a> System<'a> for RenderingSystem<'a> {
         // and draw it at the specified position.
         for (position, renderable) in rendering_data.iter() {
             // Load the image
-            let image = Image::new(self.context, renderable.path.clone()).expect("expected image");
+            let image =
+                Image::from_path(self.context, renderable.path.clone()).expect("expected image");
             let x = position.x as f32 * TILE_WIDTH;
             let y = position.y as f32 * TILE_WIDTH;
 
             // draw
             let draw_params = DrawParam::new().dest(Vec2::new(x, y));
-            graphics::draw(self.context, &image, draw_params).expect("expected render");
+            canvas.draw(&image, draw_params);
         }
 
         // Finally, present the context, this will actually display everything
         // on the screen.
-        graphics::present(self.context).expect("expected to present");
+        canvas.finish(self.context).expect("expected to present");
     }
 }
