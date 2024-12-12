56b050883428522db41097736fd06b5f1dfa743a
Olivia <olivia_github@outlook.com>
2024-12-12 19:17:58 +0000

    Update c02-01

--- code/rust-sokoban-c02-01/src/main.rs
+++ code/rust-sokoban-c02-01/src/main.rs
@@ -1,181 +1,52 @@
+/* ANCHOR: all */
 // Rust sokoban
 // main.rs
 
-use glam::Vec2;
-use ggez::{conf, event, Context, GameResult,
-    graphics::{self, DrawParam, Image}};
-use specs::{
-    join::Join, Builder, Component, ReadStorage, RunNow, System, VecStorage, World, WorldExt,
+use ggez::{
+    conf, event,
+    graphics::{self, DrawParam, Image},
+    Context, GameResult,
 };
+use glam::Vec2;
+use hecs::{Entity, World};
 
 use std::path;
 
 const TILE_WIDTH: f32 = 32.0;
 
-// Components
-#[derive(Debug, Component, Clone, Copy)]
-#[storage(VecStorage)]
+// ANCHOR: components
+#[derive(Clone, Copy)]
 pub struct Position {
     x: u8,
     y: u8,
     z: u8,
 }
 
-#[derive(Component)]
-#[storage(VecStorage)]
 pub struct Renderable {
     path: String,
 }
 
-#[derive(Component)]
-#[storage(VecStorage)]
 pub struct Wall {}
 
-#[derive(Component)]
-#[storage(VecStorage)]
 pub struct Player {}
 
-#[derive(Component)]
-#[storage(VecStorage)]
 pub struct Box {}
 
-#[derive(Component)]
-#[storage(VecStorage)]
 pub struct BoxSpot {}
 
-// Systems
-pub struct RenderingSystem<'a> {
-    context: &'a mut Context,
-}
+// ANCHOR_END: components
 
-// System implementation
-impl<'a> System<'a> for RenderingSystem<'a> {
-    // Data
-    type SystemData = (ReadStorage<'a, Position>, ReadStorage<'a, Renderable>);
-
-    fn run(&mut self, data: Self::SystemData) {
-        let (positions, renderables) = data;
-
-        // Clearing the screen (this gives us the backround colour)
-        graphics::clear(self.context, graphics::Color::new(0.95, 0.95, 0.95, 1.0));
-
-        // Get all the renderables with their positions and sort by the position z
-        // This will allow us to have entities layered visually.
-        let mut rendering_data = (&positions, &renderables).join().collect::<Vec<_>>();
-        rendering_data.sort_by_key(|&k| k.0.z);
-
-        // Iterate through all pairs of positions & renderables, load the image
-        // and draw it at the specified position.
-        for (position, renderable) in rendering_data.iter() {
-            // Load the image
-            let image = Image::new(self.context, renderable.path.clone()).expect("expected image");
-            let x = position.x as f32 * TILE_WIDTH;
-            let y = position.y as f32 * TILE_WIDTH;
-
-            // draw
-            let draw_params = DrawParam::new().dest(Vec2::new(x, y));
-            graphics::draw(self.context, &image, draw_params).expect("expected render");
-        }
-
-        // Finally, present the context, this will actually display everything
-        // on the screen.
-        graphics::present(self.context).expect("expected to present");
-    }
-}
-
+// ANCHOR: game
 // This struct will hold all our game state
 // For now there is nothing to be held, but we'll add
 // things shortly.
 struct Game {
     world: World,
 }
+// ANCHOR_END: game
 
-// This is the main event loop. ggez tells us to implement
-// two things:
-// - updating
-// - rendering
-impl event::EventHandler<ggez::GameError> for Game {
-    fn update(&mut self, _context: &mut Context) -> GameResult {
-        Ok(())
-    }
-
-    fn draw(&mut self, context: &mut Context) -> GameResult {
-        // Render game entities
-        {
-            let mut rs = RenderingSystem { context };
-            rs.run_now(&self.world);
-        }
-
-        Ok(())
-    }
-}
-
-// Register components with the world
-pub fn register_components(world: &mut World) {
-    world.register::<Position>();
-    world.register::<Renderable>();
-    world.register::<Player>();
-    world.register::<Wall>();
-    world.register::<Box>();
-    world.register::<BoxSpot>();
-}
-
-// Create a wall entity
-pub fn create_wall(world: &mut World, position: Position) {
-    world
-        .create_entity()
-        .with(Position { z: 10, ..position })
-        .with(Renderable {
-            path: "/images/wall.png".to_string(),
-        })
-        .with(Wall {})
-        .build();
-}
-
-pub fn create_floor(world: &mut World, position: Position) {
-    world
-        .create_entity()
-        .with(Position { z: 5, ..position })
-        .with(Renderable {
-            path: "/images/floor.png".to_string(),
-        })
-        .build();
-}
-
-pub fn create_box(world: &mut World, position: Position) {
-    world
-        .create_entity()
-        .with(Position { z: 10, ..position })
-        .with(Renderable {
-            path: "/images/box.png".to_string(),
-        })
-        .with(Box {})
-        .build();
-}
-
-pub fn create_box_spot(world: &mut World, position: Position) {
-    world
-        .create_entity()
-        .with(Position { z: 9, ..position })
-        .with(Renderable {
-            path: "/images/box_spot.png".to_string(),
-        })
-        .with(BoxSpot {})
-        .build();
-}
-
-pub fn create_player(world: &mut World, position: Position) {
-    world
-        .create_entity()
-        .with(Position { z: 10, ..position })
-        .with(Renderable {
-            path: "/images/player.png".to_string(),
-        })
-        .with(Player {})
-        .build();
-}
-
-// Initialize the level
+// ANCHOR: init
+// Initialize the level// Initialize the level
 pub fn initialize_level(world: &mut World) {
     const MAP: &str = "
     N N W W W W W W
@@ -209,7 +80,9 @@ pub fn load_map(world: &mut World, map_string: String) {
 
             // Figure out what object we should create
             match *column {
-                "." => create_floor(world, position),
+                "." => {
+                    create_floor(world, position);
+                }
                 "W" => {
                     create_floor(world, position);
                     create_wall(world, position);
@@ -232,10 +105,108 @@ pub fn load_map(world: &mut World, map_string: String) {
         }
     }
 }
+// ANCHOR_END: init
+
+// ANCHOR: handler
+impl event::EventHandler<ggez::GameError> for Game {
+    fn update(&mut self, _context: &mut Context) -> GameResult {
+        Ok(())
+    }
+
+    fn draw(&mut self, context: &mut Context) -> GameResult {
+        // Render game entities
+        {
+            run_rendering(&self.world, context);
+        }
+
+        Ok(())
+    }
+}
+// ANCHOR_END: handler
+
+// ANCHOR: entities
+pub fn create_wall(world: &mut World, position: Position) -> Entity {
+    world.spawn((
+        Position { z: 10, ..position },
+        Renderable {
+            path: "/images/wall.png".to_string(),
+        },
+        Wall {},
+    ))
+}
+pub fn create_floor(world: &mut World, position: Position) -> Entity {
+    world.spawn((
+        Position { z: 5, ..position },
+        Renderable {
+            path: "/images/floor.png".to_string(),
+        },
+    ))
+}
+
+pub fn create_box(world: &mut World, position: Position) -> Entity {
+    world.spawn((
+        Position { z: 10, ..position },
+        Renderable {
+            path: "/images/box.png".to_string(),
+        },
+        Box {},
+    ))
+}
+
+pub fn create_box_spot(world: &mut World, position: Position) -> Entity {
+    world.spawn((
+        Position { z: 9, ..position },
+        Renderable {
+            path: "/images/box_spot.png".to_string(),
+        },
+        BoxSpot {},
+    ))
+}
+
+pub fn create_player(world: &mut World, position: Position) -> Entity {
+    world.spawn((
+        Position { z: 10, ..position },
+        Renderable {
+            path: "/images/player.png".to_string(),
+        },
+        Player {},
+    ))
+}
+// ANCHOR_END: entities
+
+// ANCHOR: rendering_system
+fn run_rendering(world: &World, context: &mut Context) {
+    // Clearing the screen (this gives us the background colour)
+    graphics::clear(context, graphics::Color::new(0.95, 0.95, 0.95, 1.0));
+
+    // Get all the renderables with their positions and sort by the position z
+    // This will allow us to have entities layered visually.
+    let mut query = world.query::<(&Position, &Renderable)>();
+    let mut rendering_data: Vec<(Entity, (&Position, &Renderable))> = query.into_iter().collect();
+    rendering_data.sort_by_key(|&k| k.1 .0.z);
+
+    // Iterate through all pairs of positions & renderables, load the image
+    // and draw it at the specified position.
+    for (_, (position, renderable)) in rendering_data.iter() {
+        // Load the image
+        let image = Image::new(context, renderable.path.clone()).expect("expected image");
+        let x = position.x as f32 * TILE_WIDTH;
+        let y = position.y as f32 * TILE_WIDTH;
+
+        // draw
+        let draw_params = DrawParam::new().dest(Vec2::new(x, y));
+        graphics::draw(context, &image, draw_params).expect("expected render");
+    }
+
+    // Finally, present the context, this will actually display everything
+    // on the screen.
+    graphics::present(context).expect("expected to present");
+}
+// ANCHOR_END: rendering_system
+
+// ANCHOR: main
 pub fn main() -> GameResult {
-    let mut world = World::new();
-    register_components(&mut world);
-    initialize_level(&mut world);
+    let world = World::new();
 
     // Create a game context and event loop
     let context_builder = ggez::ContextBuilder::new("rust_sokoban", "sokoban")
@@ -250,3 +221,6 @@ pub fn main() -> GameResult {
     // Run the main event loop
     event::run(context, event_loop, game)
 }
+// ANCHOR_END: main
+
+/* ANCHOR_END: all */
