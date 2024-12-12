ee64890a3df60ac6c68ad3715e16093d0f6353f1
Olivia <olivia_github@outlook.com>
2024-12-12 20:51:19 +0000

    Update c02-04

---
+++ code/rust-sokoban-c02-04-a/src/components.rs
@@ -0,0 +1,22 @@
+#[derive(Clone, Copy, Eq, Hash, PartialEq)]
+pub struct Position {
+    pub x: u8,
+    pub y: u8,
+    pub z: u8,
+}
+
+pub struct Renderable {
+    pub path: String,
+}
+
+pub struct Wall {}
+
+pub struct Player {}
+
+pub struct Box {}
+
+pub struct BoxSpot {}
+
+pub struct Movable;
+
+pub struct Immovable;
---
+++ code/rust-sokoban-c02-04-a/src/entities.rs
@@ -0,0 +1,53 @@
+use crate::components::*;
+use hecs::{Entity, World};
+
+pub fn create_wall(world: &mut World, position: Position) -> Entity {
+    world.spawn((
+        Position { z: 10, ..position },
+        Renderable {
+            path: "/images/wall.png".to_string(),
+        },
+        Wall {},
+        Immovable {},
+    ))
+}
+pub fn create_floor(world: &mut World, position: Position) -> Entity {
+    world.spawn((
+        Position { z: 5, ..position },
+        Renderable {
+            path: "/images/floor.png".to_string(),
+        },
+    ))
+}
+
+pub fn create_box(world: &mut World, position: Position) -> Entity {
+    world.spawn((
+        Position { z: 10, ..position },
+        Renderable {
+            path: "/images/box.png".to_string(),
+        },
+        Box {},
+        Movable {},
+    ))
+}
+
+pub fn create_box_spot(world: &mut World, position: Position) -> Entity {
+    world.spawn((
+        Position { z: 9, ..position },
+        Renderable {
+            path: "/images/box_spot.png".to_string(),
+        },
+        BoxSpot {},
+    ))
+}
+
+pub fn create_player(world: &mut World, position: Position) -> Entity {
+    world.spawn((
+        Position { z: 10, ..position },
+        Renderable {
+            path: "/images/player.png".to_string(),
+        },
+        Player {},
+        Movable {},
+    ))
+}
