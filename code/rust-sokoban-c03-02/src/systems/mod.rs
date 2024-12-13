a6af64973f5874c685d3d0b2e2416eb38b985030
Olivia <olivia_github@outlook.com>
2024-12-13 09:49:31 +0000

    Update c03-02

--- code/rust-sokoban-c03-02/src/systems/mod.rs
+++ code/rust-sokoban-c03-02/src/systems/mod.rs
@@ -1,7 +1,3 @@
-mod input_system;
-mod rendering_system;
-mod gameplay_state_system;
-
-pub use self::input_system::InputSystem;
-pub use self::rendering_system::RenderingSystem;
-pub use self::gameplay_state_system::GameplayStateSystem;
+pub mod gameplay;
+pub mod input;
+pub mod rendering;
