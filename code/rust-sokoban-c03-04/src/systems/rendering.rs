5895a50f0ffcea3f351b95cc2c8dedd007b2feb6
Olivia <olivia_github@outlook.com>
2024-12-13 13:20:41 +0000

    Upgrade ggez to 0.9.3

--- code/rust-sokoban-c03-04/src/systems/rendering.rs
+++ code/rust-sokoban-c03-04/src/systems/rendering.rs
@@ -82,5 +82,5 @@ pub fn get_image(context: &mut Context, renderable: &Renderable, delta: Duration
 
     let image_path = renderable.path(path_index);
 
-    Image::new(context, image_path).expect("expected image")
+    Image::from_path(context, image_path).unwrap()
 }
