1be95fa7d6a4e59eafc51fccf3d20f027349fe8c
Olivia <olivia_github@outlook.com>
2024-12-13 09:47:57 +0000

    Update c03-02

--- code/rust-sokoban-c03-02-a/src/entities.rs
+++ code/rust-sokoban-c03-02-a/src/entities.rs
@@ -20,7 +20,7 @@ pub fn create_floor(world: &mut World, position: Position) -> Entity {
 pub fn create_box(world: &mut World, position: Position, colour: BoxColour) -> Entity {
     world.spawn((
         Position { z: 10, ..position },
-        Renderable::new_static(&format!("/images/box_{}.png", colour)),
+        Renderable::new_static(&format!("/images/box_{}_1.png", colour)),
         Box { colour },
         Movable {},
     ))
